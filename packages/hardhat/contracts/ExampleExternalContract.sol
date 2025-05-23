5658fb004ebab15b29e1de8c92fb8701a3e24051
Jess <panjiekenan@qq.com>
2025-05-23 21:07:26 +0800

    init

---
+++ packages/hardhat/contracts/ExampleExternalContract.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20; //Do not change the solidity version as it negatively impacts submission grading
+
+contract ExampleExternalContract {
+    // todo fix
+    bool public completed;
+
+    function complete() public payable {
+        completed = true;
+    }
+}
